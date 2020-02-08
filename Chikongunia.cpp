6a6a195bfdaca2996b83b062b5fe565b6345da24
Sukarna Paul <sukarnapaul1893@gmail.com>
2020-02-08 22:19:26 +0600

    Add files via upload

---
+++ Chikongunia.cpp
@@ -0,0 +1,177 @@
+#pragma warning(disable:4786)
+#pragma warning(disable:4996)
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+#include<ext/pb_ds/detail/standard_policies.hpp>
+#define pii pair<int,int>
+#define pll pair<long long ,long long>
+#define pli pair<long long , int>
+#define pil pair<int ,long long>
+#define pi acos(-1)
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(), a.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define MAX 300005
+#define INF 0x3f3f3f3f
+#define ff first
+#define ss second
+template <class T> inline T bigmod(T p,T e,T M){ll ret = 1LL;for(; e > 0LL; e >>= 1LL){if(e & 1LL) ret = (ret * p) % M;p = (p * p) % M;}return (T)ret;}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}   // M is prime}
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update>pref_trie;
+typedef tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+typedef long long LL;
+typedef pair<LL, LL> PLL;
+
+const PLL M=mp(1111211111, 1481481481);   ///Should be large primes
+const LL base=347;              ///Should be a prime larger than highest value
+const int N = 1e6+7;            ///Highest length of string
+
+ostream& operator<<(ostream& os, PLL hash) {
+    return os<<"("<<hash.ff<<", "<<hash.ss<<")";
+}
+
+PLL operator+ (PLL a, LL x)     {return mp(a.ff + x, a.ss + x);}
+PLL operator- (PLL a, LL x)     {return mp(a.ff - x, a.ss - x);}
+PLL operator* (PLL a, LL x)     {return mp(a.ff * x, a.ss * x);}
+PLL operator+ (PLL a, PLL x)    {return mp(a.ff + x.ff, a.ss + x.ss);}
+PLL operator- (PLL a, PLL x)    {return mp(a.ff - x.ff, a.ss - x.ss);}
+PLL operator* (PLL a, PLL x)    {return mp(a.ff * x.ff, a.ss * x.ss);}
+PLL operator% (PLL a, PLL m)    {return mp(a.ff % m.ff, a.ss % m.ss);}
+
+PLL power (PLL a, LL p) {
+    if (p==0)   return mp(1,1);
+    PLL ans = power(a, p/2);
+    ans = (ans * ans)%M;
+    if (p%2)    ans = (ans*a)%M;
+    return ans;
+}
+
+///Magic!!!!!!!
+PLL inverse(PLL a)  {
+    return power(a, (M.ff-1)*(M.ss-1)-1);
+}
+
+PLL pb[N];      ///powers of base mod M
+PLL invb;
+
+///Call pre before everything
+void hashPre() {
+    pb[0] = mp(1,1);
+    for (int i=1; i<N; i++)
+        pb[i] = (pb[i-1] * base)%M;
+    invb = inverse(pb[1]);
+}
+
+///Calculates Hash of a string
+PLL Hash (string s) {
+    PLL ans = mp(0,0);
+    for (int i=0; i<s.size(); i++)
+        ans=(ans*base + s[i])%M;
+    return ans;
+}
+
+///appends c to string
+PLL append(PLL cur, char c) {
+    return (cur*base + c)%M;
+}
+
+///prepends c to string with size k
+PLL prepend(PLL cur, int k, char c) {
+    return (pb[k]*c + cur)%M;
+}
+
+///replaces the i-th (0-indexed) character from right from a to b;
+PLL replace(PLL cur, int i, char a, char b) {
+    cur = (cur + pb[i] * (b-a))%M;
+    return (cur + M)%M;
+}
+
+///Erases c from the back of the string
+PLL pop_back(PLL hash, char c) {
+    return (((hash-c)*invb)%M+M)%M;
+}
+
+///Erases c from front of the string with size len
+PLL pop_front(PLL hash, int len, char c) {
+    return ((hash - pb[len-1]*c)%M+M)%M;
+}
+
+///concatenates two strings where length of the right is k
+PLL concat(PLL left, PLL right, int k) {
+    return (left*pb[k] + right)%M;
+}
+
+///Calculates hash of string with size len repeated cnt times
+///This is O(log n). For O(1), pre-calculate inverses
+PLL repeat(PLL hash, int len, LL cnt) {
+    PLL mul = (pb[len*cnt] - 1) * inverse(pb[len]-1);
+    mul = (mul%M+M)%M;
+    PLL ans = (hash*mul)%M;
+
+    if (pb[len].ff == 1)    ans.ff = hash.ff*cnt;
+    if (pb[len].ss == 1)    ans.ss = hash.ss*cnt;
+    return ans;
+}
+
+///Calculates hashes of all prefixes of s including empty prefix
+vector<PLL> hashList(string s) {
+    int n = s.size();
+    vector<PLL> ans(n+1);
+    ans[0] = mp(0,0);
+
+    for (int i=1; i<=n; i++)
+        ans[i] = (ans[i-1] * base + s[i-1])%M;
+    return ans;
+}
+
+///Calculates hash of substring s[l..r] (1 indexed)
+PLL substringHash(const vector<PLL> &hashlist, int l, int r) {
+    int len = (r-l+1);
+    return ((hashlist[r] - hashlist[l-1]*pb[len])%M+M)%M;
+}
+int main(){
+    IOS
+    //freopen("output.txt","w",stdout);
+    hashPre();
+    int n,m;
+    cin>>n>>m;
+    string s[n];
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+    vector<vector<PLL> > hss;
+    for(int i=0;i<n;i++){
+        string S = s[i];
+        hss.pb(hashList(S));
+    }
+    ll ans = 0;
+    map<pair<PLL,int>,ll>cnnt;
+    map<PLL,ll>cnnt2;
+    for(int i=0;i<m;i++){
+
+        for(int j=0;j<n;j++){
+            auto calc = concat(substringHash(hss[j],1,i),substringHash(hss[j],i+2,m),m-(i+1));
+            int c = s[j][i]-'a';
+            ans+= (cnnt2[calc]-cnnt[{calc,c}]);
+            cnnt[{calc,c}]++;
+            cnnt2[calc]++;
+
+
+        }
+
+        cnnt.clear();
+        cnnt2.clear();
+
+
+    }
+
+    cout<<ans<<"\n";
+}
